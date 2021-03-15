06503a26f384005ded668023e680a8fb13ba82d8
Marwa <majbrim@gmail.com>
2021-03-15 14:31:56 +0100

    added test to test palindrom function

---
+++ test.py
@@ -0,0 +1,11 @@
+import unittest
+from main import *
+
+def test_palindrome(self):
+    print(palindrome())
+    self.assertEqual(palindrome('ressasser'), 'ressasser')
+
+def test
+
+if __name__ == '__main__':
+    unittest.main(verbosity=2)
